795fef518286ca67ae1c91ef74f71244745513fe
asifh0512 <asifh0512@gmail.com>
2026-05-22 02:51:28 +0200

    Update stock_alert.py

--- stock_alert.py
+++ stock_alert.py
@@ -1,20 +1,16 @@
 import os
-from resend import Resend
+import resend
 
-# Henter API-key fra GitHub Secrets
-api_key = os.environ["RESEND_API_KEY"]
-
-resend = Resend(api_key)
+resend.api_key = os.environ["RESEND_API_KEY"]
 
 def send_test_email():
-    response = resend.emails.send({
+    response = resend.Emails.send({
         "from": "Pokemon Alert <onboarding@resend.dev>",
-        "to": ["asifh0512@gmail.com"],
+        "to": ["DIN_EPOST_HER@gmail.com"],
         "subject": "TEST - Pokemon stock alert",
-        "html": "<p>🎉 Test: Resend fungerer!</p>"
+        "html": "<p>🎉 Resend fungerer nå!</p>"
     })
 
     print(response)
 
 send_test_email()
-print("Ferdig")
